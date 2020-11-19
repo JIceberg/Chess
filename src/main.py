e365f77da8defd59d3303cd0b335ec688fde41ec
JIceberg <jisenberg3@gatech.edu>
2020-11-18 20:28:02 -0500

    add svg rendering

--- src/main.py
+++ src/main.py
@@ -1,14 +1,18 @@
 import chess
-import parse_input as parser
+import chess.svg
+from parse_input import *
+from svglib.svglib import svg2rlg
+from reportlab.graphics import renderPM
 
 board = chess.Board()
 board.reset()
-print(board)
+
+'''
 while True:
     if board.is_checkmate():
         print("Checkmate")
         break
-    st_in = parser.parse_to_valid_chess_move(input())
+    st_in = parse_to_valid_chess_move(input())
     mov = chess.Move.null() if st_in.lower() == "skip" else chess.Move.from_uci(st_in)
     if mov == chess.Move.null() or mov in board.legal_moves:
         board.push(mov)
@@ -17,3 +21,26 @@ while True:
         print("Cannot make this move - you are in check.")
     else:
         print("Illegal move")
+'''
+
+f = open("generated/board.svg", "w+")
+f.write(chess.svg.board(board))
+f.close()
+
+drawing = svg2rlg("generated/board.svg")
+renderPM.drawToFile(drawing, "generated/board.png", fmt="PNG")
+
+from tkinter import *
+gui = Tk()
+gui.title("Chess")
+
+from PIL import Image, ImageTk
+img = Image.open('generated/board.png')
+pimg = ImageTk.PhotoImage(img)
+size = img.size
+
+frame = Canvas(gui, width=size[0], height=size[1])
+frame.pack()
+frame.create_image(0, 0, anchor='nw', image=pimg)
+
+gui.mainloop()
