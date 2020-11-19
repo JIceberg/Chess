84c01c90d655ba83b079a0bbbe86d11700ad7059
JIceberg <jisenberg3@gatech.edu>
2020-11-18 21:35:24 -0500

    add gui functionality

--- src/main.py
+++ src/main.py
@@ -3,44 +3,59 @@ import chess.svg
 from parse_input import *
 from svglib.svglib import svg2rlg
 from reportlab.graphics import renderPM
+from tkinter import *
+
+def __create_board_png():
+    f = open("generated/board.svg", "w+")
+    f.write(chess.svg.board(board))
+    f.close()
+    drawing = svg2rlg("generated/board.svg")
+    renderPM.drawToFile(drawing, "generated/board.png", fmt="PNG")
 
 board = chess.Board()
 board.reset()
+__create_board_png()
 
-'''
-while True:
+gui = Tk()
+gui.title("Chess")
+move = StringVar()  # global
+
+from PIL import Image, ImageTk
+
+def run():
     if board.is_checkmate():
         print("Checkmate")
-        break
-    st_in = parse_to_valid_chess_move(input())
+        return
+    st_in = parse_to_valid_chess_move(move.get())
     mov = chess.Move.null() if st_in.lower() == "skip" else chess.Move.from_uci(st_in)
     if mov == chess.Move.null() or mov in board.legal_moves:
         board.push(mov)
-        print(board)
+        __create_board_png()
+        global _pimg
+        _pimg = ImageTk.PhotoImage(Image.open('generated/board.png'))
+        frame.itemconfig(game_img, image = _pimg)
     elif board.is_check():
         print("Cannot make this move - you are in check.")
     else:
         print("Illegal move")
-'''
 
-f = open("generated/board.svg", "w+")
-f.write(chess.svg.board(board))
-f.close()
+__create_board_png()
 
-drawing = svg2rlg("generated/board.svg")
-renderPM.drawToFile(drawing, "generated/board.png", fmt="PNG")
+input_label = Label(gui, text="Your Turn")
+input_label.pack(side = TOP)
+section = Frame(gui)
+section.pack(side = TOP)
+user_input = Entry(section, textvariable=move)
+user_input.pack(side = LEFT)
+play_button = Button(section, text='Move', command=run)
+play_button.pack(side = RIGHT)
 
-from tkinter import *
-gui = Tk()
-gui.title("Chess")
-
-from PIL import Image, ImageTk
 img = Image.open('generated/board.png')
 pimg = ImageTk.PhotoImage(img)
 size = img.size
 
 frame = Canvas(gui, width=size[0], height=size[1])
+game_img = frame.create_image(0, 0, anchor='nw', image=pimg)
 frame.pack()
-frame.create_image(0, 0, anchor='nw', image=pimg)
 
 gui.mainloop()
